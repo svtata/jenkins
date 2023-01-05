54ed2bac31695d148e6b4e2a7bd178d05ced79a8
Tatiana Sverchkova <swerchkova.tatiana@gmail.com>
2023-01-05 21:16:19 +0400

    Added registration test

---
+++ src/test/java/demoqa/TestBase.java
@@ -0,0 +1,25 @@
+package demoqa;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @BeforeEach
+    void AddListener(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments(){
+
+    }
+}
