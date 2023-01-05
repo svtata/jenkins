9b3a239b33e052355ff15ffdedbe7f898df971d6
Tatiana Sverchkova <swerchkova.tatiana@gmail.com>
2023-01-05 22:10:47 +0400

    Added remote url

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -11,6 +11,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @BeforeEach
