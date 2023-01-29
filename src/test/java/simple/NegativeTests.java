07378478bd8563cfd805478d01470a2393d56714
Tatiana Sverchkova <swerchkova.tatiana@gmail.com>
2023-01-29 15:37:48 +0400

    Added allure notifications

--- src/test/java/simple/NegativeTests.java
+++ src/test/java/simple/NegativeTests.java
@@ -1,9 +1,11 @@
 package simple;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Tag("simple")
 public class NegativeTests {
     @Test
     void test1() {
--- src/test/java/simple/PositiveTests.java
+++ src/test/java/simple/PositiveTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Tag("smoke")
+@Tag("simple")
 public class PositiveTests {
 
     @Test
